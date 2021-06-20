afac6375dcc5b90cbdbb510015d13b7960e190a1
Tay Kai Yang <taykaiyang556@gmail.com>
2021-06-21 00:43:33 +0800

    added docker-compose architecture

--- fastapi/app/misc/testApi.py
+++ fastapi/app/misc/testApi.py
@@ -4,7 +4,7 @@ from yolov5s import Yolov5s
 import json
 
 model = Yolov5s()
-url = 'http://127.0.0.5:8000'
+url = 'http://127.0.0.4:8000'
 
 def postImg():
     global url
@@ -24,7 +24,7 @@ def postImg():
 def getResult():
     global url
 
-    task_id = '328625b7-e036-4147-a804-3e3088014fc3'
+    task_id = '30378c82-94b1-4f5e-af53-e255aeca136a'
     result_url = url + '/gymEqpOD/result/' + task_id
     res = requests.get(result_url)
     
@@ -32,6 +32,7 @@ def getResult():
     img = cv2.imread(img_path)
 
     data = res.json()
+    print(data)
     pred = data['preds'][0]
     bbox = pred['bbox']
     cat = pred['cat']
