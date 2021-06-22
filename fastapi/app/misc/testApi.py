1747f683e2c58064642eb777fc83b3bc0aba8ab3
Tay Kai Yang <taykaiyang556@gmail.com>
2021-06-22 21:26:59 +0800

    updated yaml files

--- fastapi/app/misc/testApi.py
+++ fastapi/app/misc/testApi.py
@@ -4,7 +4,7 @@ from yolov5s import Yolov5s
 import json
 
 model = Yolov5s()
-url = 'http://127.0.0.4:8000'
+url = 'http://127.0.0.1:8000'
 
 def postImg():
     global url
@@ -24,15 +24,16 @@ def postImg():
 def getResult():
     global url
 
-    task_id = '30378c82-94b1-4f5e-af53-e255aeca136a'
+    task_id = '8ed85f7b-6685-46df-bffa-fa1afe5c23c2'
     result_url = url + '/gymEqpOD/result/' + task_id
     res = requests.get(result_url)
     
     img_path = '../../../tf_serving/cat.jpg'
     img = cv2.imread(img_path)
 
+    print('test', res.text)
     data = res.json()
-    print(data)
+    
     pred = data['preds'][0]
     bbox = pred['bbox']
     cat = pred['cat']
